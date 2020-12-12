78856161d393a45ede2d6ea83349a88f7734992e
Maninder.Mike <maninder.singh@outlook.in>
2020-12-12 22:45:20 +0300

    setting

--- gomongo.go
+++ gomongo.go
@@ -1,19 +1,70 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
 )
 
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
-    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-    if err != nil{
-        log.Fatal(err)
-    }
-    collection := client.Database("testing").Collection("numbers")
-    fmt.Printf("%v, %T", collection, collection)
+
+	uri := "mongodb+srv://Maninder:Maninder@123@cluster0.mongodb.net/test?w=majority"
+
+	// Set client options
+	// clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI(uri)
+
+	// Connect to MongoDB
+	client, e := mongo.Connect(context.TODO(), clientOptions)
+	CheckError(e)
+
+	// Check the connection
+	e = client.Ping(context.TODO(), nil)
+	CheckError(e)
+
+	// get collection as ref
+	collection := client.Database("testdb").Collection("people")
+
+	// insert
+	john := Person{"John", 24}
+	jane := Person{"Jane", 27}
+	ben := Person{"Ben", 16}
+
+	_, e = collection.InsertOne(context.TODO(), john)
+	CheckError(e)
+
+	persons := []interface{}{jane, ben}
+	_, e = collection.InsertMany(context.TODO(), persons)
+	CheckError(e)
+
+	// update
+	filter := bson.D{{"name", "John"}}
+
+	update := bson.D{
+		{"$set", bson.D{
+			{"age", 26},
+		}},
+	}
+
+	_, e = collection.UpdateOne(context.TODO(), filter, update)
+	CheckError(e)
+
+	// find
+	var res Person
+	e = collection.FindOne(context.TODO(), filter).Decode(&res)
+	fmt.Println(res)
+
+	// delete
+	_, e = collection.DeleteMany(context.TODO(), bson.D{{}})
+	CheckError(e)
+}
+
+func CheckError(e error) {
+	if e != nil {
+		fmt.Println(e)
+	}
 }
